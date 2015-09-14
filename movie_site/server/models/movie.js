2cf060d0011439fdfe3d69995425fc6deaf33e3f
三尸神 <1327653237@qq.com>
2015-09-14 22:29:31 +0800

    添加控制器，抽取router里地代码实现放入控制器

--- movie_site/server/models/movie.js
+++ movie_site/server/models/movie.js
@@ -1,32 +1,90 @@
+/** 
+ * @author  sanshishen
+ * @email   1327653237@qq.com
+ * @date    2015-09-11 13:45:39
+ * @version 1.0.0
+ */
+
 /**
  * 数据库设计
  *
  * Schema 	 模式 \			  \
  * Model  	 模型 -- Mongoose  -- Mongodb
  * Documents 文档 /            /
- * 
- * @author  sanshishen
- * @email   1327653237@qq.com
- * @date    2015-09-11 13:45:39
- * @version 1.0.0
  */
-var mongoose = require('mongoose');
+
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
 /**
  * MovieSchema 模式定义
  */
-var MovieSchema = new mongoose.Schema({
-	title: String,
-	director: String,
+var MovieSchema = new Schema({
 	country: String,
+	director: String,
+	flash: String,
 	language: String,
-	year: Number,
-	summary: String
+	// 录入或者更新的时间
+	meta: {
+		createAt: {
+			type: Date,
+			// 默认值
+			default: Date.now()
+		},
+		updateAt: {
+			type: Date,
+			default: Date.now()
+		}
+	},
+	poster: String,
+	summary: String,
+	title: String,
+	year: Number
 });
+// 该方法在每次保存前都会执行
+MovieSchema.pre('save', function(next) {
+	if (this.isNew) {
+		// 如果该数据是新的数据
+		this.meta.createAt = this.updateAt = Date.now();
+	} else {
+		// 如果该数据已经存在，只更新upateAt属性
+		this.meta.updateAt = Date.now();
+	}
+	// 调用next，让存储流程继续
+	next();
+});
+/**
+ * 设置一些静态方法
+ * 所有的静态方法都不会直接与数据库进行交互的，
+ * 只有经过编译和实例化之后才会具有这个方法。
+ */
+MovieSchema.statics = {
+	/**
+	 * 用来取出目前数据库里地所有数据
+	 * @param  {Function} cb [description]
+	 * @return {[type]}      [description]
+	 */
+	fetch: function(cb) {
+		return this
+			.find({})
+			.sort('meta.updateAt') // 排序
+			.exec(cb);
+	},
+	/**
+	 * 更具id查找一条记录
+	 * @param  {[type]}   id [description]
+	 * @param  {Function} cb [description]
+	 * @return {[type]}      [description]
+	 */
+	findById: function(id, cb) {
+		return this
+			.findOne({_id: id})
+			.exec(cb);
+	}
+};
 /**
  * Model 编译模型
  */
-var Movie = mongoose.model('Movie', MovieSchema);
-module.exports = Movie;
+module.exports = mongoose.model('Movie', MovieSchema);
 
 /**
  * Documents 文档实例化
@@ -78,4 +136,4 @@ movie.save(function(err) {
 		.remove({_id: res.params._id}, function(err, movie) {
 			if (err) console.log(err);
 		});
-});
+});*/
--- movie_site/server/routes/router.js
+++ movie_site/server/routes/router.js
@@ -5,56 +5,16 @@
  * @date    2015-09-07 17:04:58
  * @version 1.0.0
  */
+// 使用严格模式
+'use strict';
 module.exports = function(app) {
+	var movie = require('../controllers/movie')();
 	// 首页数据
-	app.get('/index', function(req, res) {
-		var data = {
-			title: 'imooc 首页',
-			movies: [{
-				_id: '01',
-				title: '机械战警',
-				poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-			}, {
-				_id: '02',
-				title: '机械战警',
-				poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-			}, {
-				_id: '03',
-				title: '机械战警',
-				poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-			}, {
-				_id: '04',
-				title: '机械战警',
-				poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-			}, {
-				_id: '05',
-				title: '机械战警',
-				poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-			}, {
-				_id: '06',
-				title: '机械战警',
-				poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-			}]
-		};
-		res.json(data);
-	});
+	app.route('/index')
+		.get(movie.all);
 	// 详情页数据
-	app.get('/movie/:_id', function(req, res) {
-		var _id = req.params._id;
-		res.json({
-			title: '影片详情页',
-			movie: {
-				director: '何塞·帕蒂利亚',
-				country: '美国',
-				title: '机械战警',
-				year: 2014,
-				poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-				language: '英语',
-				flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-				summary: '2028年，专事军火开发的机器人公司Omni Corp.生产了大量装备精良的机械战警，他们被投入到惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼 饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼 饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什 饰）和儿子大卫却再难从他身上感觉亲人的温暖。感知到妻儿的痛苦，墨菲决心向策划杀害自己的犯罪头子展开反击……'
-			}
-		});
-	});
+	app.route('/movie/:_id')
+		.get(movie.findById);
 	// 后台录入页面
 	app.get('/admin/add', function(req, res) {
 		res.json({
@@ -72,20 +32,9 @@ module.exports = function(app) {
 		});
 	});
 	// 后台列表页
-	app.get('/admin/list', function(req, res) {
-		res.json({
-			title: '后台列表页',
-			movies: [{
-				_id: '01',
-				director: '何塞·帕蒂利亚',
-				country: '美国',
-				title: '机械战警',
-				year: 2014,
-				language: '英语',
-				flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf'
-			}]
-		});
-	});
+	app.route('/admin/list')
+		.get(movie.list);
+		
 	app.get('*', function(req, res) {
 		// 加载单视图文件
 		// 前端将会由angular控制页面的改变
